ea8d44e7d386d06804bc4c29641ba37a4ff168a0
Lisa <sunmengqing@123feng.com>
2016-04-25 22:41:58 +0800

    add js and css files to the repository

---
+++ popup.js
@@ -0,0 +1,27 @@
+smq-popup {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.2);
+  z-index: 2000;
+  display: block;
+}
+
+smq-popup .window {
+  margin: 200px auto;
+  width: 500px;
+  min-height: 100px;
+  background: rgba(255,255,255,1);
+  border-radius: 10px;
+}
+
+smq-popup .close {
+  margin: 0 10px 0 0;
+  float: right;
+  color: rgba(200,200,200,1);
+  font-size: 20px;
+  cursor: pointer;
+  font-family: sans-serif;
+}
